2c9b37c5a2b451e2a520ad7830cb315747e7a5f5
Vona Csaba <vcsneu@gmail.com>
2021-04-18 12:51:43 +0200

    managing data done

--- angular-tutorial/src/app/cart.service.ts
+++ angular-tutorial/src/app/cart.service.ts
@@ -1,11 +1,12 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root',
 })
 export class CartService {
   items = [];
-  constructor() {}
+  constructor(private http: HttpClient) {}
 
   addToCart(product) {
     this.items.push(product);
@@ -18,5 +19,11 @@ export class CartService {
   clearItems() {
     this.items = [];
     return this.items;
+  }
+
+  getShippingPrices() {
+    return this.http.get<{ type: string; price: number }[]>(
+      '/assets/shipping.json'
+    );
   }
 }
